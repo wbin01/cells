0b7fd5fd20449ed1e66fe310210028dc65c23d48
wbin01 <wildenalisson@outlook.com>
2024-12-22 17:42:02 -0300

    Signal doc

--- cells/signal.py
+++ cells/signal.py
@@ -5,24 +5,46 @@ from .core.coresignal import CoreSignal
 class Signal(object):
     """Signal object."""
     def __init__(self):
-        """Class constructor."""
+        """Class constructor.
+
+        Signals an event:
+
+            MyObj:
+                obj_signal = Signal()
+
+                def obj_call(self):
+                    obj_signal.emit()
+
+
+            my_obj = MyObj()
+            my_obj.obj_signal.connect(lamba: print('Signal has been emitted'))
+
+        When a signal is emitted, it performs the connected function.
+        """
         self.__signal = CoreSignal()
 
     @property
-    def value(self) -> str:
-        """Value of any type passed to the class constructor."""
+    def value(self) -> any:
+        """Signal value.
+
+            my_signal = self.obj_signal
+            signal_value = my_signal.value
+            self.my_signal.connect(lambda: print(signal_value))
+        """
         return self.__signal.value
 
     @value.setter
-    def value(self, value: str) -> None:
+    def value(self, value: any) -> None:
         self.__signal.value = value
 
-    def connect(self, function, *args) -> None:
+    def connect(self, callback: callable) -> None:
         """Function to be executed.
+
+            my_obj.obj_signal.connect(self.my_function)
 
         :param function: Function to be executed when the signal is sent.
         """
-        self.__signal.callback(function)
+        self.__signal.callback(callback)
 
     def emit(self) -> None:
         """Send this signal.
