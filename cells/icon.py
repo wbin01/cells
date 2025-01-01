b14c1d5cca98c7da31dc664a3fdd5682ec885c10
wbin01 <wildenalisson@outlook.com>
2025-01-01 01:59:21 -0300

    Icon

--- cells/icon.py
+++ cells/icon.py
@@ -8,22 +8,49 @@ from __feature__ import snake_case
 class Icon(object):
     """Icon."""
     def __init__(
-            self, icon: str = None, width: int = 22, height: int = 22,
+            self,
+            path: str = None,
+            fallback_path: str = None,
+            width: int = 22,
+            height: int = 22,
             *args, **kwargs) -> None:
-        """Class constructor."""
+        """Class constructor.
+
+        The icon is rendered from the path of a passed file, or from the name 
+        of an icon in the current operating system, such as "folder-download". 
+        To find out all the system icon names, see the "freedesktop.org" 
+        specification:
+            https://specifications.freedesktop.org/icon-naming-spec/latest/
+
+        Although the Freedesk specification is for Linux, we are making it 
+        compatible with Windows. If an icon is not found, it will not give an 
+        error, but it will also not render any image, so to ensure an image is 
+        rendered use a file path as a fallback (fallback_path).
+
+        :param path: Icon path or icon name from system, like "folder-download"
+        :param width: Integer with the value of the icon width.
+        :param height: Integer with the value of the icon height.
+        """
         super().__init__(*args, **kwargs)
-        self.__path = icon
+        self.__path = path
+        self.__fallback_path = fallback_path if fallback_path else ''
         self.__width = width
         self.__height = height
 
         if not os.path.isfile(self.__path):
             self.__icon = QtGui.QIcon.from_theme(self.__path)
+
+            if not self.__icon.has_theme_icon(self.__path):
+                self.__icon = QtGui.QIcon(self.__fallback_path)
         else:
             self.__icon = QtGui.QIcon(self.__path)
 
     @property
     def height(self) -> int:
-        """..."""
+        """Returns the height of the Icon.
+
+        Pass a new integer value to update the height.
+        """
         return self.__height
 
     @height.setter
@@ -32,17 +59,24 @@ class Icon(object):
 
     @property
     def path(self) -> str:
-        """..."""
+        """Icon path.
+
+        Pass a new path to update the icon image.
+        """
         return self.__path
 
     @path.setter
     def path(self, path: str) -> None:
         self.__path = path
         self.__icon = QtGui.QIcon(self.__path)
+        # self.__icon.add_pixmap(path)
 
     @property
     def width(self) -> int:
-        """..."""
+        """Returns the Widget width.
+
+        Pass a new integer value to update the Icon.
+        """
         return self.__width
 
     @width.setter
