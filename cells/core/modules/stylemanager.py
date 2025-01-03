43b71865ff0ddf038af50e2e4e0b4adeaf5b30be
wbin01 <wildenalisson@outlook.com>
2025-01-03 18:15:43 -0300

    style borders

--- cells/core/modules/stylemanager.py
+++ cells/core/modules/stylemanager.py
@@ -105,6 +105,30 @@ class StyleManager(object):
                     'accent_green', self.__accent_green).replace(
                     'accent_blue', self.__accent_blue)
                 border = style_parser.border_str_to_list(border)
+            if 'border_top' in style[group_key]:
+                border_top = style[group_key]['border_top'].replace(
+                    'accent_red', self.__accent_red).replace(
+                    'accent_green', self.__accent_green).replace(
+                    'accent_blue', self.__accent_blue)
+                border_top = border_top.split('px')
+            if 'border_right' in style[group_key]:
+                border_right = style[group_key]['border_right'].replace(
+                    'accent_red', self.__accent_red).replace(
+                    'accent_green', self.__accent_green).replace(
+                    'accent_blue', self.__accent_blue)
+                border_right = border_right.split('px')
+            if 'border_bottom' in style[group_key]:
+                border_bottom = style[group_key]['border_bottom'].replace(
+                    'accent_red', self.__accent_red).replace(
+                    'accent_green', self.__accent_green).replace(
+                    'accent_blue', self.__accent_blue)
+                border_bottom = border_bottom.split('px')
+            if 'border_left' in style[group_key]:
+                border_left = style[group_key]['border_left'].replace(
+                    'accent_red', self.__accent_red).replace(
+                    'accent_green', self.__accent_green).replace(
+                    'accent_blue', self.__accent_blue)
+                border_left = border_left.split('px')
             if 'border_radius' in style[group_key]:
                 border_radius = style[group_key]['border_radius']
                 border_radius = style_parser.border_radius_str_to_list(
@@ -143,30 +167,46 @@ class StyleManager(object):
                 qss += f'background: url({background_image});'  # no-repeat
             if 'border' in style[group_key]:
                 qss += (
-                    f'  border-top: {border[0]}px solid {border[4]};\n'
-                    f'  border-right: {border[1]}px solid {border[4]};\n'
-                    f'  border-bottom: {border[2]}px solid {border[4]};\n'
-                    f'  border-left: {border[3]}px solid {border[4]};\n')
+                    f' border-top: {border[0]}px solid {border[4]};\n'
+                    f' border-right: {border[1]}px solid {border[4]};\n'
+                    f' border-bottom: {border[2]}px solid {border[4]};\n'
+                    f' border-left: {border[3]}px solid {border[4]};\n')
+            if 'border_top' in style[group_key]:
+                qss += (
+                    f' border-top: {border_top[0].strip()}px'
+                    f' solid {border_top[1].strip()};\n')
+            if 'border_right' in style[group_key]:
+                qss += (
+                    f' border-right: {border_right[0].strip()}px'
+                    f' solid {border_right[1].strip()};\n')
+            if 'border_bottom' in style[group_key]:
+                qss += (
+                    f' border-bottom: {border_bottom[0].strip()}px'
+                    f' solid {border_bottom[1].strip()};\n')
+            if 'border_left' in style[group_key]:
+                qss += (
+                    f' border-left: {border_left[0].strip()}px'
+                    f' solid {border_left[1].strip()};\n')
             if 'border_radius' in style[group_key]:
                 qss += (
-                    f'  border-top-left-radius: {border_radius[0]}px;\n'
-                    f'  border-top-right-radius: {border_radius[1]}px;\n'
-                    f'  border-bottom-left-radius: {border_radius[3]}px;\n'
-                    f'  border-bottom-right-radius: {border_radius[2]}px;\n')
+                    f' border-top-left-radius: {border_radius[0]}px;\n'
+                    f' border-top-right-radius: {border_radius[1]}px;\n'
+                    f' border-bottom-left-radius: {border_radius[3]}px;\n'
+                    f' border-bottom-right-radius: {border_radius[2]}px;\n')
             if 'color' in style[group_key]:
                 qss += f'  color: {color};\n'
             if 'margin' in style[group_key] or 'Frame' in group_key:
                 qss += (
-                    f'  margin-top: {margin[0]}px;\n'
-                    f'  margin-right: {margin[1]}px;\n'
-                    f'  margin-bottom: {margin[2]}px;\n'
-                    f'  margin-left: {margin[3]}px;\n')
+                    f' margin-top: {margin[0]}px;\n'
+                    f' margin-right: {margin[1]}px;\n'
+                    f' margin-bottom: {margin[2]}px;\n'
+                    f' margin-left: {margin[3]}px;\n')
             if 'padding' in style[group_key]:
                 qss += (
-                    f'  padding-top: {padding[0]}px;\n'
-                    f'  padding-right: {padding[1]}px;\n'
-                    f'  padding-bottom: {padding[2]}px;\n'
-                    f'  padding-left: {padding[3]}px;\n')
+                    f' padding-top: {padding[0]}px;\n'
+                    f' padding-right: {padding[1]}px;\n'
+                    f' padding-bottom: {padding[2]}px;\n'
+                    f' padding-left: {padding[3]}px;\n')
             qss += '}\n'
 
 
--- cells/core/modules/stylemanagerparser.py
+++ cells/core/modules/stylemanagerparser.py
@@ -3,12 +3,20 @@
 
 def border_str_to_list(border: str) -> list:
     """Parser border str.
-
-    '1 1 1 1 rgba(0, 0, 0, 1)'  ->  ['1', '1', '1', 'rgba(0, 0, 0, 1)']
-    '1 1 1 1 #FF00F3'           ->  ['1', '1', '1', '#FF00F3']
+    
+    '1px rgba(0, 0, 0, 1)'              ->  ['1', '1', '1', 'rgba(0, 0, 0, 1)']
+    '1px 1px 1px 1px rgba(0, 0, 0, 1)'  ->  ['1', '1', '1', 'rgba(0, 0, 0, 1)']
+    '1px 1px 1px 1px #FF00F3'           ->  ['1', '1', '1', '#FF00F3']
     """
-    border = border.strip().replace('  ', ' ')
-    n1, n2, n3, n4, *c = border.replace('px', '').split()
+    # border = border.strip().replace('  ', ' ')
+    # n1, n2, n3, n4, *c = border.replace('px', '').split()
+
+    bd = border.strip().replace('  ', ' ').split('px')
+    if len(bd) == 2:
+        n1, n2, n3, n4, *c = bd[0], bd[0], bd[0], bd[0], bd[1]
+    else:
+        n1, n2, n3, n4, *c = bd
+
     if '#' in c:
         pass
     else:
@@ -19,36 +27,43 @@ def border_str_to_list(border: str) -> list:
 def border_list_to_str(border: list) -> str:
     """Parser border list.
 
-    ['1', '1', '1', '1', 'rgba(0, 0, 0, 1)'] -> '1 1 1 1 rgba(0, 0, 0, 1)'
-    ['1', '1', '1', '1', '#FF00F3']          -> '1 1 1 1 #FF00F3'
+    ['1', '1', '1', '1', 'rgba(0,0,0,1)'] -> '1px 1px 1px 1px rgba(0,0,0,1)'
+    ['1', '1', '1', '1', '#FF00F3']       -> '1px 1px 1px 1px #FF00F3'
     """
-    return ' '.join(border)
+    return 'px '.join(border)
 
 def border_radius_str_to_list(border: str) -> list:
     """Parser border radius str.
 
-    '1 1 1 1'  ->  ['1', '1', '1', '1']
+    '1px 1px 1px 1px'  ->  ['1', '1', '1', '1']
     """
-    border = border.strip().replace('  ', ' ')
-    return border.replace('px', '').split()
+    # border = border.strip().replace('  ', ' ')
+    # return border.replace('px', '').split()
+    bd = border.strip().replace('  ', ' ').split('px')
+    if len(bd) == 2:
+        n1, n2, n3, n4 = bd[0], bd[0], bd[0], bd[0]
+    else:
+        n1, n2, n3, n4, _ = bd
+
+    return [n1, n2, n3, n4]
 
 def border_radius_list_to_str(border: list) -> str:
     """Parser border radius list.
 
-    ['1', '1', '1', '1']  ->  '1 1 1 1'
+    ['1', '1', '1', '1']  ->  '1px 1px 1px 1px'
     """
-    return ' '.join(border)
+    return 'px '.join(border)
 
 def margin_padding_str_to_list(margin_padding: str) -> list:
     """Parser margin or padding str.
 
-    '1 1 1 1'  ->  ['1', '1', '1', '1']
+    '1px 1px 1px 1px'  ->  ['1', '1', '1', '1']
     """
     return border_radius_str_to_list(margin_padding)
 
 def margin_padding_list_to_str(margin_padding: list) -> str:
     """Parser margin or padding list.
 
-    ['1', '1', '1', '1']  ->  '1 1 1 1'
+    ['1', '1', '1', '1']  ->  '1px 1px 1px 1px'
     """
     return border_radius_list_to_str(margin_padding)
